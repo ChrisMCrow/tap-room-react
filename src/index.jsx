465078340edbb633437000f575f9febdfe291fc3
Chris Crow <chrismcrow@gmail.com>
2018-11-16 09:28:06 -0800

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import { HashRouter } from 'react-router-dom';
+
+
+const render = (Component) => {
+  ReactDOM.render(
+    <HashRouter>
+      <Provider store={store}>
+        <Component/>
+      </Provider>
+    </HashRouter>,
+    document.getElementById('react-app-root')
+  );
+};
+
+render(App);
+
+/*eslint-disable */
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    render(App);
+  });
+}
